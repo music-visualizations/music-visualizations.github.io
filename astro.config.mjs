11b8848a5e96da58bf53fb59713402867dd18d70
Xavier Yuhan Liu <xavieryuhanliu@outlook.com>
2024-07-04 23:38:17 +0800

    add github page configure

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://xavieryuhanliu.github.io",
+  base: "/music-visualizations",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
